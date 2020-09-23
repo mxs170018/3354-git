7a33db43c37d1cc2cc02fcbd3aac1202552a91b4
Manuel Salado Alvarado <mxs170018@utdallas.edu>
2020-09-23 15:38:20 -0500

    Changed the method to accept the subtraction of numbers

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -6,19 +6,37 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
         try {
+            
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
+            
             System.err.println("Please provide valid integers to be added");
         }
     }
 
     private static int addArguments(String[] args) {
-        int var = args.length;
-        int total = 0; 
-        for(int i = 0; i < var; i++){
-            total += Integer.valueOf(args[i]);
+        int total = 0;
+        int var = 0;
+        
+        if(args[0].equals("-")){
+            
+            var = args.length; 
+            for(int i = 1; i < var; i++){
+                
+                total -= Integer.valueOf(args[i]);
+            }
+            
+        }else{
+            var = args.length;
+            for(int i = 0; i < var; i++){
+                
+                total += Integer.valueOf(args[i]);
+            }
         }
+            
+    
+
         return total; 
     }
 }
