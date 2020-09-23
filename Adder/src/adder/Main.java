b5515785c7132cc5e6d69b4129815d656c36795a
Manuel Salado Alvarado <mxs170018@utdallas.edu>
2020-09-23 14:58:57 -0500

    Changed the number of arguments to be added
    Usually the program adds two integers that were passed as arguments, but now it adds all of the arguments passed.
    
    I updated the error message to reflect these changes

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,17 +1,24 @@
-package adder;
+//package adder;
+
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         try {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide valid integers to be added");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        int var = args.length;
+        int total = 0; 
+        for(int i = 0; i < var; i++){
+            total += Integer.valueOf(args[i]);
+        }
+        return total; 
     }
 }
